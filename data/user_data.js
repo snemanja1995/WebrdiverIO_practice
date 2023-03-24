b3a42d80b1ee079a3dde2735ddd615d15baa73ef
Nemanja <neco.stanisic@gmail.com>
2023-03-24 14:26:48 +0100

    Added package.json and user_data

---
+++ data/user_data.js
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv' 
+dotenv.config()
+
+export default {} = {
+    username: "standard_user",
+    password: process.env.password,
+    firstName: "Nemanja",
+    lastName: "Stanisic",
+    number: (24000)
+
+}
